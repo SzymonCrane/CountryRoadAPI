558e88550f3ff02c9e27305cf40e82b2af50de68
Szymon Żuraw <szymon.zurawit@wp.pl>
2021-11-20 17:37:18 +0100

    Add project files.

---
+++ CountryAPI/Controllers/CountryRoadController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CountryRoadController : ControllerBase
+    {
+        private static readonly string[] Countries = new[]
+        {
+            "CAN", "USA", "MEX", "BLZ", "GTM", "SLV", "HND", "NIC", "CRI", "PAN"
+        };
+
+        private readonly ILogger<CountryRoadController> _logger;
+
+        public CountryRoadController(ILogger<CountryRoadController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "CAN";
+        }
+    }
+}
---
+++ CountryAPI/CountryRoad.cs
@@ -0,0 +1,8 @@
+﻿namespace CountryAPI
+{
+    public class CountryRoad
+    {
+        public string CountryCode { get; set; }
+        public string CountryName { get; set; }
+    }
+}
