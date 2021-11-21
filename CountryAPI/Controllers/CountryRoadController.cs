cd471bf01174ecc32acfb2687d53a4f2110b7e53
Szymon Żuraw <szymon.zurawit@wp.pl>
2021-11-21 15:24:16 +0100

    Finished coding, added all probabilities and false exception. TODO: make it more automatic

--- CountryAPI/Controllers/CountryRoadController.cs
+++ CountryAPI/Controllers/CountryRoadController.cs
@@ -24,18 +24,148 @@ namespace CountryAPI.Controllers
         }
 
         [HttpGet("{roadTo}")]
-        public JsonDocument GetJson(string roadTo)
+        public ActionResult<CountryRoad> GetJson(string roadTo)
         {
-            var destination = new CountryRoad();
             if (roadTo == "CAN")
             {
-            destination = new CountryRoad{
-                Destination = roadTo,
-                Countries = { ListOfCountries[1], ListOfCountries[0] }
-            };
+                var destination = new CountryRoad
+                {
+                    Destination = roadTo,
+                    Countries = new List<string> {
+                    "USA",
+                    "CAN"
+                }
+                };
+                //Console.Write(destination);
+                //var destinationJson = JsonDocument.Parse(JsonSerializer.Serialize(destination));
+                return destination;
             }
-            var destinationJson = JsonDocument.Parse(JsonSerializer.Serialize(destination));
-            return destinationJson;
+            else if (roadTo == "MEX")
+            {
+                var destination = new CountryRoad
+                {
+                    Destination = roadTo,
+                    Countries = new List<string>
+                    {
+                        "USA",
+                        "MEX"
+                    }
+                };
+                return destination;
+            }
+            else if (roadTo == "BLZ")
+            {
+                var destination = new CountryRoad
+                {
+                    Destination = roadTo,
+                    Countries = new List<string>
+                    {
+                        "USA",
+                        "MEX",
+                        "BLZ"
+                    }
+                };
+                return destination;
+            }
+            else if (roadTo == "GTM")
+            {
+                var destination = new CountryRoad
+                {
+                    Destination = roadTo,
+                    Countries = new List<string>
+                    {
+                        "USA",
+                        "MEX",
+                        "GTM"
+                    }
+                };
+                return destination;
+            }
+            else if (roadTo == "SLV")
+            {
+                var destination = new CountryRoad
+                {
+                    Destination = roadTo,
+                    Countries = new List<string>
+                    {
+                        "USA",
+                        "MEX",
+                        "GTM",
+                        "SLV"
+                    }
+                };
+                return destination;
+            }
+            else if (roadTo == "HND")
+            {
+                var destination = new CountryRoad
+                {
+                    Destination = roadTo,
+                    Countries = new List<string>
+                    {
+                        "USA",
+                        "MEX",
+                        "GTM",
+                        "HND"
+                    }
+                };
+                return destination;
+            }
+            else if (roadTo == "NIC")
+            {
+                var destination = new CountryRoad
+                {
+                    Destination = roadTo,
+                    Countries = new List<string>
+                    {
+                        "USA",
+                        "MEX",
+                        "GTM",
+                        "HND",
+                        "NIC"
+                    }
+                };
+                return destination;
+            }
+            else if (roadTo == "CRI")
+            {
+                var destination = new CountryRoad
+                {
+                    Destination = roadTo,
+                    Countries = new List<string>
+                    {
+                        "USA",
+                        "MEX",
+                        "GTM",
+                        "HND",
+                        "NIC",
+                        "CRI"
+                    }
+                };
+                return destination;
+            }
+            else if (roadTo == "PAN")
+            {
+                var destination = new CountryRoad
+                {
+                    Destination = roadTo,
+                    Countries = new List<string>
+                    {
+                        "USA",
+                        "MEX",
+                        "GTM",
+                        "HND",
+                        "NIC",
+                        "CRI",
+                        "PAN"
+                    }
+                };
+                return destination;
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
