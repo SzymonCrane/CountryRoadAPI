6516c8154368d74cb24344ee26f8c96b56565c54
Szymon Żuraw <szymon.zurawit@wp.pl>
2021-11-25 17:43:20 +0100

    Cleaned usings from files

--- CountryAPI/Controllers/CountryRoadController.cs
+++ CountryAPI/Controllers/CountryRoadController.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using System.Text.Json;
 using CountryAPI.Interfaces.Services;
 
 namespace CountryAPI.Controllers
--- CountryAPI/Services/CountryRoadService.cs
+++ CountryAPI/Services/CountryRoadService.cs
@@ -1,9 +1,7 @@
 ﻿using CountryAPI.Interfaces.Repositories;
 using CountryAPI.Interfaces.Services;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace CountryAPI.Services
 {
